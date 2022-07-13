4c97c18d29218f31745942eb18d3542d9c498ea6
Deepak Singh <deepak.s.singh@globallogic.com>
2022-07-13 12:57:43 +0530

    changed in settings

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from core.models import Item
+cat = ['Shirt', 'Casualwear', 'sport']
+label = ['primary','secondary', 'danger']
+cat = ['Shirt', 'Outwear', 'Sport wear']
+price = [30, 40, 50, 33, 35, 25]
+discount = [2,1,3]
+catg = ['S','SW','OW']
+lab = ['P','S','D']
+for i in range(1000000):
+    random.shuffle(cat)
+    random.shuffle(label)
+    random.shuffle(price)
+    random.shuffle(discount)
+    xt = ['casual.jpeg','party.jpg','shirt.jpg','sport.jpg']
+    random.shuffle(xt)
+    random.shuffle(catg)
+    random.shuffle(lab)
+    im = Item(title=cat[1]+str(i),price=price[1],discount_price=discount[1],
+    	category=catg[1],label=lab[1],slug=cat[1]+str(i),description='testing',image=xt[1])
+    im.save()
--- djecommerce/settings/development.py
+++ djecommerce/settings/development.py
@@ -37,9 +37,17 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecom_site_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
